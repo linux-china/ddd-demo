0d00cdd53d0ea2002dd89574e1a8062b61122495
linux_china <linux_china@hotmail.com>
2015-12-07 09:54:20 +0800

    DDD demo base application test

---
+++ src/test/java/org/mvnsearch/ddd/DddDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.ddd;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * DDD demo base application test
+ *
+ * @author linux_china
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DddDemoApplication.class)
+public abstract class DddDemoApplicationTests {
+
+}
